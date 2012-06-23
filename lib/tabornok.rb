c63a1732e5d15d40bdf168b62bf7f7f755eb1dd9
Jackneill <general.jackneill@gmail.com>
2012-06-23 16:32:05 +0200

    Added multithreading support and command line commands support

--- lib/tabornok.rb
+++ lib/tabornok.rb
@@ -2,10 +2,11 @@
 
 require "socket"
 require "colorize"
+require "thread"
 
 class Tabornok
 
-	def initialize server, port, nick
+    def initialize server, port, nick
         puts "Tabornok IRC Bot and Framework".yellow
         puts "Copyright (C) 2012 Jackneill".yellow
         puts "This program comes with ABSOLUTELY NO WARRANTY; for details type see <http://www.gnu.org/licenses/>.".yellow
@@ -15,17 +16,18 @@ class Tabornok
         @server = server
         @port	= port
         @nick 	= nick
-	end
+    end
 
 	def connect
 		@tcpsocket = TCPSocket.open @server, @port
 
 		auth
-		read
+		thread_read_tcpsocket = Thread.new read_tcpsocket
+        thread_read           = Thread.new read
 	end
 
 	def auth
-		sendIrcMsg "NICK " + @nick
+        sendIrcMsg "NICK #{@nick}"
         sendIrcMsg "USER Tabornok 0 * Tabornok"
         sendIrcMsg "JOIN #hun_bot"
     end
@@ -34,24 +36,28 @@ class Tabornok
     	@tcpsocket.puts msg
     end
 
+    def sendMsg chan, msg
+        @tcpsocket.puts "PRIVMSG #{chan} #{msg}"
+    end
+
     def log lvl, msg
         case lvl
         when 1
-            puts "[1] ".red + msg
+            puts "[1] #{msg}".red
         when 2
-            puts "[2] ".yellow + msg
+            puts "[2] #{msg}".yellow
         when 3
-            puts "[3] ".blue
+            puts "[3] #{msg}".blue
         when 4
-            puts "[4] ".green + msg
+            puts "[4] #{msg}".green
         when 5
-            puts "[5] " + msg
+            puts "[5] #{msg}"
         else
-            puts "[ERROR] " + msg
+            puts "[ERROR] #{msg}"
         end
     end
 
-    def read
+    def read_tcpsocket
     	until @tcpsocket.eof? do
     		parse @tcpsocket.gets
     	end
@@ -66,6 +72,18 @@ class Tabornok
         when "PRIVMSG"
             case data[2]
             when "AUTH"
+            end
+        end
+    end
+
+    def read
+        while true
+            cmd = gets.chomp
+            #cmd.inspect
+
+            if cmd[0..2] == "say"
+                puts cmd
+                sendMsg "#hun_bot", cmd
             end
         end
     end
