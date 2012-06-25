4db1dec6af92751ed06e01ec8501a30916faac65
Jackneill <general.jackneill@gmail.com>
2012-06-25 19:01:24 +0200

    Fixed unknown OS bug in win, thanks to Megaxxx

--- lib/tabornok/utils.rb
+++ lib/tabornok/utils.rb
@@ -5,7 +5,8 @@ class Utils
 	def check_os
 		if RUBY_PLATFORM.downcase.include?("darwin")
 			return "mac"
-		elsif RUBY_PLATFORM.downcase.include?("mswin")
+		elsif RUBY_PLATFORM.downcase.include?("mswin") ||
+			RUBY_PLATFORM.downcase.include?("mingw")
 			return "win"
 		elsif RUBY_PLATFORM.downcase.include?("linux")
 			return "linux"
